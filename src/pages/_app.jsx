a7e72a7a84a5ba8d683d3a2de65a48c4712b6f10
Gegy Faskal Nur Hamdani <gegyhamdani@gmail.com>
2023-09-26 10:48:15 +0700

    add meta head

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -6,6 +6,26 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <title>ORCA | React with Unity</title>
+        <meta
+          content="width=device-width, initial-scale=1, maximum-scale=5"
+          name="viewport"
+        />
+        <meta content="text/html; charset=utf-8" httpEquiv="Content-Type" />
+        <meta content="id" name="language" />
+        <meta content="id" name="geo.country" />
+        <meta content="Indonesia" name="geo.placename" />
+
+        <meta content="ORCA | React with Unity" key="title" name="title"/>
+        <meta
+          content="Create your own React app with Unity as the game engine"
+          name="description"
+        />
+        <meta content="react, next, unity, engine" name="keywords" />
+        <meta content="ORCA | React with Unity" name="author" />
+
+        <meta content="website" key="ogType" property="og:type" />
+        <meta content="ORCA | React with Unity" key="ogTitle" property="og:title" />
+        <meta content="ORCA | React with Unity" key="ogSiteName" property="og:site_name" />
       </Head>
       <Component {...pageProps} />
     </>
